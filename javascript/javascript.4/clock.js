b55f34ac26c9ad8b9d082b367ae15417d69aad1e
whdgkr1127 <79003007+whdgkr1127@users.noreply.github.com>
2021-04-27 23:39:51 +0900

    practice.Update9

--- javascript/javascript.4/clock.js
+++ javascript/javascript.4/clock.js
@@ -7,18 +7,12 @@ const clockContainer = document.querySelector(".js-clock"),
         const hours = date.getHours();
         const seconds = date.getSeconds();
         clockTitle.innerText = `${hours < 10 ? `0${hours}`:hours}:${minute < 10 ? `0${minute}`:minute}:${seconds < 10 ? `0${seconds}`:seconds }`;
-   
-        if(seconds >= 50){
-            clockTitle.style.color = "red";
-        }else{
-            clockTitle.style.color = "black";
-        }
-        
     }
+
+  
 
 function init(){
     getTime();
     setInterval(getTime,1000);
 }
 init();
-console.dir(clockTitle);
